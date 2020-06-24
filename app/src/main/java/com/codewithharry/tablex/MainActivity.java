46deccfb52731a07c7fa7b362084551d43d6f7ce
snehsingh2020 <snehsingh2020@gmail.com>
2020-06-24 11:43:48 +0530

    Version 1

---
+++ app/src/main/java/com/codewithharry/tablex/MainActivity.java
@@ -0,0 +1,56 @@
+package com.codewithharry.tablex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    ListView listView;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView);
+        listView=findViewById(R.id.listView);
+        seekBar=findViewById(R.id.seekBar);
+        seekBar.setMax(20);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Toast.makeText(MainActivity.this, "populate "+i, Toast.LENGTH_SHORT).show();
+                populate(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    public void populate(int table){
+        ArrayList<String> mulTable=new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            mulTable.add(table+" X "+i+" = "+table*i);
+        }
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,mulTable);
+        listView.setAdapter(arrayAdapter);
+        textView.setText("Multiplication TAble of "+table);
+    }
+}
